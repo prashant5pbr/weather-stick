f6468c2783f24079b95ff9267f3eb1fb37e8ef2b
Prashant <220758338+prashant5pbr@users.noreply.github.com>
2026-08-04 19:07:52 +0530

    Fixed cross-origin issue by referencing allowedDevOrigins

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
+  allowedDevOrigins: ['192.168.1.102'],
 };
 
 export default nextConfig;
