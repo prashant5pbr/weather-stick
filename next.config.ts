3b42e9070e469527e46654e56102e688eeb22090
Prashant <220758338+prashant5pbr@users.noreply.github.com>
2026-08-07 15:47:33 +0530

    Moved allowedDevOrigins to env configuration.

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
-  allowedDevOrigins: ['192.168.1.102'],
+  allowedDevOrigins: process.env.ALLOWED_DEV_ORIGINS?.split(',') ?? [],
 };
 
 export default nextConfig;
