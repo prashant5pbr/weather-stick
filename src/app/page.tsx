cb147c656cbe5fd4d07710382ed324b3e7f393b8
Prashant <220758338+prashant5pbr@users.noreply.github.com>
2026-08-09 20:23:58 +0530

    Fixed the hydration issue for the homepage using dynamic import.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,14 @@
-import { Home } from '@/components/home';
+'use client';
+
+import dynamic from 'next/dynamic';
+
+// dynamic is a next.js provided function to avoid ssr for client side front-end apps
+// {ssr: false} ensures that the component is rendered entirely on the client side (csr)
+// With named imports, import() fetches the object containing all the imports from the given file
+// then() unpacks the named export object and extracts the desired component
+const Home = dynamic(() => import('@/components/home').then((mod) => mod.Home), {
+  ssr: false,
+});
 
 export default function HomePage() {
   return <Home />;
