a2f63dfd1fabd31cfca5f16c8fa4d3fe4bfab837
Prashant <220758338+prashant5pbr@users.noreply.github.com>
2026-08-18 22:02:15 +0530

    Changed the background style.

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -3,10 +3,11 @@ import styles from '@/css/page.module.css';
 // Component to create background style
 const Background = function () {
   return (
-    <div className={styles.aurora} aria-hidden="true">
-      <span className={`${styles.blob} ${styles.blobOne}`} />
-      <span className={`${styles.blob} ${styles.blobTwo}`} />
-      <span className={`${styles.blob} ${styles.blobThree}`} />
+    <div className={styles.canvas} aria-hidden="true">
+      <div className={styles.sky} />
+      <div className={styles.sun} />
+      <div className={styles.beams} />
+      <div className={styles.haze} />
     </div>
   );
 };
