8b914fb84aca0196f97156072c1d5400a8df6f20
Prashant <220758338+prashant5pbr@users.noreply.github.com>
2026-08-23 13:13:58 +0530

    Added property onPlaceSelect.

--- src/types/form-data.types.tsx
+++ src/types/form-data.types.tsx
@@ -1,3 +1,5 @@
+import type { PlaceSuggestion } from '@/types/place-suggestion.types';
+
 // Shape of the data for the forms in the weather page
 interface DataProps {
   inputDraft: { [key: string]: string };
@@ -5,6 +7,7 @@ interface DataProps {
   setIsEmpty: React.Dispatch<React.SetStateAction<boolean>>;
   onChange: (field: 'place' | 'date', e: React.ChangeEvent<HTMLInputElement>) => void;
   onBlur: (field: 'place' | 'date', e: React.FocusEvent<HTMLInputElement>) => void;
+  onPlaceSelect: (place: PlaceSuggestion) => void;
   onSubmit: React.SubmitEventHandler<HTMLFormElement>;
 }
 
